b837b9e2c3aac0622df2625ed8c2c123d1bf3766
Mehedi Hasan <you@example.com>
2022-10-27 07:50:58 +0600

    readme partially completed

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAcAzgFGx5g7gPWXQliPS_kpAz0AMj5ZpE",
-  authDomain: "cs-learners.firebaseapp.com",
-  projectId: "cs-learners",
-  storageBucket: "cs-learners.appspot.com",
-  messagingSenderId: "1066185874514",
-  appId: "1:1066185874514:web:5cccefc02699d8c5d9938b"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
 
 // Initialize Firebase
